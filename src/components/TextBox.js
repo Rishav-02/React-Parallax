c54194ff6611e83c5c1495b73aae398f5b95bff0
Rishav <rishavsinha380@gmail.com>
2023-01-29 11:52:51 +0530

    First Detailing about PARALLAX

---
+++ src/components/TextBox.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TextBox = () => {
+    return(
+        <div className="text-box">
+            <h3>Parallax</h3>
+            <h4 id="first">"Parallax" is a displacement or difference in the apparent position of an object viewed along two different lines of sight and is measured by the angle or semi-angle of inclination between those two lines.
+            </h4>
+        </div>
+    )
+}
+
+export default TextBox;
