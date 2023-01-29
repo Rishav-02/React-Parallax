2532089373282d3a74de20ab19238d82a1c54b1e
Rishav <rishavsinha380@gmail.com>
2023-01-29 11:55:38 +0530

    Third detail NOTE of parallax scrolling

---
+++ src/components/Text2.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Text2 = () => {
+    return(
+        <div className="text-box">
+            <h3>Note:</h3>
+            <h4 id="third">Parallax scrolling does not always work on mobile devices/smart phones. However, you can use media queries to turn off the effect on mobile devices.
+            </h4>
+        </div>
+    )
+}
+
+export default Text2;
