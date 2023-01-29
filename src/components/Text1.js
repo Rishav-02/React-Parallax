44bc68204d696f17e66e36d1508c719dffc94d8e
Rishav <rishavsinha380@gmail.com>
2023-01-29 11:54:14 +0530

    Second detail about PARALLAX SCROLLING

---
+++ src/components/Text1.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Text1 = () => {
+    return(
+        <div className="text-box">
+            <h3>Parallax Scrolling</h3>
+            <h4 id="second">"Parallax scrolling" is a scrolling technique used in computer graphics in which background images move more slowly than images in the foreground, creating the illusion of depth and immersion. It is often used in video games.
+            </h4>
+        </div>
+    )
+}
+
+export default Text1;
