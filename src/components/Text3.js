ab52da0aa4ab7a0c3d8a23ab5a7507516c826803
Rishav <rishavsinha380@gmail.com>
2023-01-30 15:17:28 +0530

    Fourth detail about Space

---
+++ src/components/Text3.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Text3 = () => {
+    return(
+        <div className="text-box">
+            <h3>Space</h3>
+            <h4 id="fourth">
+            Space is the boundless three-dimensional extent in which objects and events have relative position and direction.In classical physics, physical space is often conceived in three linear dimensions, although modern physicists usually consider it, with time, to be part of a boundless four-dimensional continuum known as spacetime.
+            </h4>
+        </div>
+    )
+}
+
+export default Text3;
